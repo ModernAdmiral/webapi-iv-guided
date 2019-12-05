0a8b99cd9e4db2391977ef7a009f3615a50377f7
Jackson McComas <jackson.mccomas@gmail.com>
2019-12-05 12:20:51 -0600

    changes more

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const server = require("./api/server.js");
-
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4004;
 
 server.listen(4004, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
